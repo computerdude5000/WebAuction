887dc22839595db7bf985bbd796b69ebea3976e4
Connor James <ConnorNJames@gmail.com>
2012-03-21 12:46:19 -0400

    Updated for Craftbukkit 1.1 RB6+
    Also includes silly modifications that IntelliJ did

--- WebAuction/src/me/exote/webauction/listeners/WebAuctionServerListener.java
+++ WebAuction/src/me/exote/webauction/listeners/WebAuctionServerListener.java
@@ -1,18 +1,18 @@
 package me.exote.webauction.listeners;
 
 import me.exote.webauction.WebAuction;
-
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.server.PluginEnableEvent;
-import org.bukkit.event.server.ServerListener;
 
-public class WebAuctionServerListener extends ServerListener {
+public class WebAuctionServerListener implements Listener {
 	private WebAuction plugin;
 
 	public WebAuctionServerListener(WebAuction webAuction) {
 		this.plugin = webAuction;
 	}
 
-	@Override
+	@EventHandler
 	public void onPluginEnable(PluginEnableEvent event) {
 		if (plugin.economy != null) {
 			if (plugin.economy.isEnabled()) {
